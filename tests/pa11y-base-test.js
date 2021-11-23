1de22e00e4762066e71112169ffca2ef68d38db0
NageshJoy007 <nagesh.joy007@gmail.com>
2021-11-23 22:12:50 +0530

    adding user actions and standard to the test

--- tests/pa11y-base-test.js
+++ tests/pa11y-base-test.js
@@ -3,3 +3,12 @@ const pa11y = require('pa11y');
 pa11y('https://yourwesite.com/').then((results) => {
     console.log(results)
 });
+
+pa11y('https://yourwesite.com/', {
+  actions:[
+            `click element [href="/account/login"]`,
+            `wait for element #customer_email to be visible`],
+  standard: 'WCAG2AA'
+    }).then((results) => {
+       console.log(results)
+});
