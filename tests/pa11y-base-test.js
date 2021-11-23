96169dc73ba4270ac8041ef658e0561f863d953e
NageshJoy007 <nagesh.joy007@gmail.com>
2021-11-23 22:24:11 +0530

    added rules to ignore and screenshot capability

--- tests/pa11y-base-test.js
+++ tests/pa11y-base-test.js
@@ -8,7 +8,11 @@ pa11y('https://yourwesite.com/', {
   actions:[
             `click element [href="/account/login"]`,
             `wait for element #customer_email to be visible`],
-  standard: 'WCAG2AA'
+  standard: 'WCAG2AA',
+  ignore:[
+            'WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.A.NoContent',
+            'WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail'],
+  screenCapture: `${__dirname}/login-screen-capture.png`
     }).then((results) => {
        console.log(results)
 });
